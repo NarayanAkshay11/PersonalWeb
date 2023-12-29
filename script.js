1c7d57b1e244123bdb4c1959016447687b83d38c
Akshay Narayanan B <90465549+NarayanAkshay11@users.noreply.github.com>
2023-12-29 11:11:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,14 @@ function displayCurrentTime() {
     currentTimeElement.textContent = `Current Time: ${hours}:${minutes}`;
 }
 
-function toggleMode() {
+function toggleMode(mode) {
     const body = document.body;
-    body.classList.toggle("dark-mode");
+
+    if (mode === 'light') {
+        body.classList.remove('dark-mode');
+    } else if (mode === 'dark') {
+        body.classList.add('dark-mode');
+    }
 }
 
 window.onload = function() {
