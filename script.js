cf8fa17ceaa0fd45c16b34c28b48b4efcd451ded
Akshay Narayanan B <90465549+NarayanAkshay11@users.noreply.github.com>
2023-12-29 10:48:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
-
-function displayLocationInfo() {
-    const countryTimezoneElement = document.getElementById('country-timezone');
-    
-    
-    const userCountry = new Intl.DateTimeFormat(undefined, {hour12: false}).resolvedOptions().timeZone;
-    
-    
+function displayCurrentTime() {
+    const currentTimeElement = document.getElementById('current-time');
     const currentTime = new Date();
     const hours = currentTime.getHours().toString().padStart(2, '0');
     const minutes = currentTime.getMinutes().toString().padStart(2, '0');
-    
-    
-    countryTimezoneElement.textContent = `Country: ${userCountry}, Current Time: ${hours}:${minutes}`;
+    currentTimeElement.textContent = `Current Time: ${hours}:${minutes}`;
 }
 
-
 window.onload = function() {
-    displayLocationInfo();
+    displayCurrentTime();
 };
